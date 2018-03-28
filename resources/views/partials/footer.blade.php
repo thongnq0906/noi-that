57f9967b3cdcda4ffa09266b6efd40187e3a6cbb
thongnq0906 <thongnq0906@gmail.com>
2018-03-28 11:18:32 +0700

    static-page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,100 @@
+<footer class="footer">
+  <div class="brand-logo ">
+    <div class="container">
+      <div class="slider-items-products">
+        <div id="brand-logo-slider" class="product-flexslider hidden-buttons">
+          <div class="slider-items slider-width-col6"> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo1.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo2.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo3.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo4.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo5.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo6.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo1.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+            <!-- Item -->
+            <div class="item"> <a href="#x"><img src="images/b-logo4.png" alt="Image"></a> </div>
+            <!-- End Item --> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-middle container">
+    <div class="col-md-3 col-sm-4">
+      <div class="footer-logo"><a href="index.html" title="Logo"><img src="images/footer-logo.png" alt="logo"></a></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus diam arcu. </p>
+      <div class="payment-accept">
+        <div><img src="images/payment-1.png" alt="payment"> <img src="images/payment-2.png" alt="payment"> <img src="images/payment-3.png" alt="payment"> <img src="images/payment-4.png" alt="payment"></div>
+      </div>
+    </div>
+    <div class="col-md-2 col-sm-4">
+      <h4>Shopping Guide</h4>
+      <ul class="links">
+        <li class="first"><a href="#" title="How to buy">How to buy</a></li>
+        <li><a href="faq.html" title="FAQs">FAQs</a></li>
+        <li><a href="#" title="Payment">Payment</a></li>
+        <li><a href="#" title="Shipment&lt;/a&gt;">Shipment</a></li>
+        <li><a href="delivery.html" title="delivery">Delivery</a></li>
+        <li class="last"><a href="#" title="Return policy">Return policy</a></li>
+      </ul>
+    </div>
+    <div class="col-md-2 col-sm-4">
+      <h4>Style Advisor</h4>
+      <ul class="links">
+        <li class="first"><a title="Your Account" href="login.html">Your Account</a></li>
+        <li><a title="Information" href="#">Information</a></li>
+        <li><a title="Addresses" href="#">Addresses</a></li>
+        <li><a title="Addresses" href="#">Discount</a></li>
+        <li><a title="Orders History" href="#">Orders History</a></li>
+        <li class="last"><a title=" Additional Information" href="#">Additional Information</a></li>
+      </ul>
+    </div>
+    <div class="col-md-2 col-sm-4">
+      <h4>Information</h4>
+      <ul class="links">
+        <li class="first"><a href="sitemap.html" title="Site Map">Site Map</a></li>
+        <li><a href="#/" title="Search Terms">Search Terms</a></li>
+        <li><a href="#" title="Advanced Search">Advanced Search</a></li>
+        <li><a href="contact_us.html" title="Contact Us">Contact Us</a></li>
+        <li><a href="#" title="Suppliers">Suppliers</a></li>
+        <li class=" last"><a href="#" title="Our stores" class="link-rss">Our stores</a></li>
+      </ul>
+    </div>
+    <div class="col-md-3 col-sm-4">
+      <h4>Contact us</h4>
+      <div class="contacts-info">
+        <address>
+        <i class="add-icon">&nbsp;</i>123 Main Street, Anytown, <br>
+        &nbsp;CA 12345  USA
+        </address>
+        <div class="phone-footer"><i class="phone-icon">&nbsp;</i> +1 800 123 1234</div>
+        <div class="email-footer"><i class="email-icon">&nbsp;</i> <a href="mailto:support@magikcommerce.com">support@magikcommerce.com</a> </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom container">
+    <div class="col-sm-5 col-xs-12 coppyright"> &copy; 2015 Magikcommerce. All Rights Reserved.</div>
+    <div class="col-sm-7 col-xs-12 company-links">
+      <ul class="links">
+        <li><a href="#" title="Magento Themes">Magento Themes</a></li>
+        <li><a href="#" title="Premium Themes">Premium Themes</a></li>
+        <li><a href="#" title="Responsive Themes">Responsive Themes</a></li>
+        <li class="last"><a href="#" title="Magento Extensions">Magento Extensions</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
